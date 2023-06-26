d03878e3fa06cf7d06518d73f930f283a57143ae
jnnngs <>
2023-06-26 17:17:21 +0100

    python MF_Provision_Region.py vsam

--- pkg/gobankdemo/gobankdemo.go
+++ pkg/gobankdemo/gobankdemo.go
@@ -25,14 +25,23 @@ func Vsam(input []string) (result string) {
 		fmt.Printf("*---[VSAM] Parameter provided: %v\n", input)
 	}
 
-	fmt.Printf("*---[VSAM] Clone BankDemo GitHub.com Repo\n")
+	//Clone BankDemo Repo
+	fmt.Printf("*---[VSAM] Start Clone BankDemo GitHub.com Repo\n")
 	var repo = "https://github.com/MicroFocus/BankDemo.git"
 	cmd = exec.Command("git", "clone", repo, "--progress")
 	if err := cmd.Run(); err != nil {
 		return err.Error()
 	}
-	fmt.Printf("*---[VSAM] Done\n")
+	fmt.Printf("*---[VSAM] End Clone BankDemo GitHub.com Repo\n")
 
+	//Run python MF_Provision_Region.py vsam
+	fmt.Printf("*---[VSAM] Start python MF_Provision_Region.py vsam\n")
+
+	cmd = exec.Command("python", "MF_Provision_Region.py", "vsam")
+	if err := cmd.Run(); err != nil {
+		return err.Error()
+	}
+	fmt.Printf("*---[VSAM] End python MF_Provision_Region.py vsam\n")
 	fmt.Printf("*---[VSAM] Completed\n")
 	return "Done"
 }
